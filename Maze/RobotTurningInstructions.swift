e8616e0aab02e35f3ad9645b0cad4ec61dc5fb24
Eric Nguyen <ericnguyen@erics-imac.local>
2016-02-18 22:06:25 -0600

    Update two functions: randomlyRotateRightOrLeft and continueStraightOrRotate

--- Maze/RobotTurningInstructions.swift
+++ Maze/RobotTurningInstructions.swift
@@ -14,12 +14,31 @@ extension ControlCenter {
         let randomNumber = arc4random() % 2
         
         // You may want to paste your Part 1 implementation of randomlyRotateRightOrLeft(robot: ComplexRobotObject) here
+        if randomNumber == 0{
+            robot.rotateRight()
+            
+        }
+        
+        if randomNumber == 1{
+            robot.rotateLeft()
+        }
+
+        
     }
     
     func continueStraightOrRotate(robot: ComplexRobotObject) {
         let randomNumber = arc4random() % 2
         
         // You may want to paste your Part 1 implementation of continueStraightOrRotate(robot: ComplexRobotObject) here
+        if randomNumber == 0{
+            randomlyRotateRightOrLeft(robot)
+        }
+        
+        if randomNumber == 1{
+            robot.move()
+        }
+
+        
         
     }
 }
